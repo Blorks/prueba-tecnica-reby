c09eaa312f4c67426de0554e42f9f8e2643175c4
Daniel Alvarez Valero <blorkszb@gmail.com>
2022-04-24 20:08:00 +0200

    feat: 3 test for testing edge use cases on the API

--- test/integration/ride/rideController_test.go
+++ test/integration/ride/rideController_test.go
@@ -16,11 +16,12 @@ import (
 
 var serverConfig integration.ServerConfig
 
+/* -------------- Init Ride Tests --------------- */
 func Test_Init_Ride_Ok(t *testing.T) {
 	if !serverConfig.ServerUp {
-		serverConfig.SetupServer(false)
+		serverConfig.SetupServer(false, false, false, false)
 	} else {
-		serverConfig.LoadSQLFiles(false)
+		serverConfig.LoadSQLFiles(false, false, false, false)
 	}
 
 	body := `{"idUser": 1, "idVehicle": 1}`
@@ -50,11 +51,60 @@ func Test_Init_Ride_Ok(t *testing.T) {
 	assert.Equal(t, 1, dtoResponse.IdVehicle)
 }
 
+func Test_Init_Ride_UserBalanceTooLow(t *testing.T) {
+	if !serverConfig.ServerUp {
+		serverConfig.SetupServer(false, true, false, false)
+	} else {
+		serverConfig.LoadSQLFiles(false, true, false, false)
+	}
+
+	body := `{"idUser": 1, "idVehicle": 1}`
+	request, err := http.NewRequest("POST", "http://localhost:8080/rides", strings.NewReader(body))
+
+	response := serverConfig.ExecuteRequest(request)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	assert.Equal(t, http.StatusForbidden, response.Code)
+
+	responseByte, _ := io.ReadAll(response.Body)
+	responseMsg := string(responseByte)
+
+	assert.Equal(t, "The user's balance is too low to start the ride\n", responseMsg)
+}
+
+func Test_Init_Ride_VehicleInUse(t *testing.T) {
+	if !serverConfig.ServerUp {
+		serverConfig.SetupServer(false, false, true, false)
+	} else {
+		serverConfig.LoadSQLFiles(false, false, true, false)
+	}
+
+	body := `{"idUser": 1, "idVehicle": 1}`
+	request, err := http.NewRequest("POST", "http://localhost:8080/rides", strings.NewReader(body))
+
+	response := serverConfig.ExecuteRequest(request)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	assert.Equal(t, http.StatusForbidden, response.Code)
+
+	responseByte, _ := io.ReadAll(response.Body)
+	responseMsg := string(responseByte)
+
+	assert.Equal(t, "The used vehicle is not available at this time\n", responseMsg)
+}
+
+/* -------------- Finish Ride Tests --------------- */
 func Test_Finish_Ride_Ok(t *testing.T) {
 	if !serverConfig.ServerUp {
-		serverConfig.SetupServer(true)
+		serverConfig.SetupServer(true, false, false, false)
 	} else {
-		serverConfig.LoadSQLFiles(true)
+		serverConfig.LoadSQLFiles(true, false, false, false)
 	}
 
 	request, err := http.NewRequest("POST", "http://localhost:8080/rides/1/finish", nil)
@@ -82,4 +132,27 @@ func Test_Finish_Ride_Ok(t *testing.T) {
 	assert.Equal(t, 1, dtoResponse.IdUser)
 	assert.Equal(t, 1, dtoResponse.IdVehicle)
 	assert.NotEmpty(t, dtoResponse.Cost)
+}
+
+func Test_Finish_Ride_RideFinished(t *testing.T) {
+	if !serverConfig.ServerUp {
+		serverConfig.SetupServer(true, false, false, true)
+	} else {
+		serverConfig.LoadSQLFiles(true, false, false, true)
+	}
+
+	request, err := http.NewRequest("POST", "http://localhost:8080/rides/1/finish", nil)
+
+	response := serverConfig.ExecuteRequest(request)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	assert.Equal(t, http.StatusForbidden, response.Code)
+
+	responseByte, _ := io.ReadAll(response.Body)
+	responseMsg := string(responseByte)
+
+	assert.Equal(t, "The ride that is trying to end is already over\n", responseMsg)
 }
